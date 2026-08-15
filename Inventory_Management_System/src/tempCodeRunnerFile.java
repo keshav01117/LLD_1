f0fbba437787984c5b263a788f8b0e5151f2407d
Keshav <Keshav.01117@gmail.com>
2026-08-15 22:45:41 +0530

    Remove compiled files from tracking

--- Inventory_Management_System/src/tempCodeRunnerFile.java
+++
@@ -1,4 +0,0 @@
- // System.out.println("All Items in Inventory:");
-        // for (Item item : inventory.getAllItems()) {
-        //     System.out.println(item.getId() + ": " + item.getName() + ": " + item.getPrice() + " x " + item.getQuantity());
-        // }
